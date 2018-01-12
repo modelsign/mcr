cb4add796bf0993d2536863fbd82dc16fbf3a686
budblack <budblack@qq.com>
2018-01-12 23:39:40 +0800

    思绪迷走在夜雾中, 指尖流过的字句刺激着我的前额皮质.

--- src/lib/controller/CameraController.ts
+++ src/lib/controller/CameraController.ts
@@ -1,3 +1,50 @@
+import TWEEN from '@tweenjs/tween.js';
+
+const TIME_SECEND = 1000;
+
+
 export class CameraController {
 
+    _isMoving: Boolean = false;
+    _tCameraPosition: TWEEN.Tween;
+    _tCameraDirection: TWEEN.Tween;
+    camera: THREE.PerspectiveCamera;
+
+    constructor(camera: THREE.PerspectiveCamera) {
+        this.camera = camera;
+
+
+        this._tCameraPosition = new TWEEN.Tween(camera.position);
+    }
+
+    /**
+     * 平滑移动相机到指定位置, 并朝向某个方向
+     * @param {Vector3} position
+     * @param {Vector3} direction
+     * @param {number} timeout
+     * @return {Promise<PerspectiveCamera>}
+     */
+    move(position: THREE.Vector3, direction: THREE.Vector3 = null, timeout: number = TIME_SECEND): Promise<THREE.PerspectiveCamera> {
+        return new Promise((resolve) => {
+            let camera = this.camera, direct = camera.getWorldDirection,
+                tCameraPosition = this._tCameraPosition,
+                tCameraDirection = new TWEEN.Tween(direct)
+            ;
+            if (this._isMoving) {
+                tCameraPosition.stop();
+            }
+            this._isMoving = true;
+            tCameraPosition.easing(TWEEN.Easing.Quartic.Out)
+                .to(position, timeout)
+                .onComplete(() => {
+                    resolve(tCamera);
+                })
+                .start();
+            tCameraDirection.easing(TWEEN.Easing.Quartic.Out).to(direction, timeout).start();
+        });
+    }
+
+    fly(positions: THREE.Vector3[], timeout: number = positions.length): Promise<THREE.PerspectiveCamera> {
+
+    }
 }
---
+++ src/lib/controller/SceneController.ts
@@ -0,0 +1,15 @@
+export class SceneController {
+
+    scene: THREE.Scene;
+
+    constructor(scene: THREE.Scene) {
+
+    }
+
+
+    elementsFind(name: string): Promise<Element[]> {
+
+    }
+
+
+}
